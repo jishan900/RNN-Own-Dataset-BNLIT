523fe84cd6028e10799d72868cfe64c4cdfdc4b0
jishan900 <jishan900@gmail.com>
2019-01-24 01:55:25 +0600

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# own_data_rnn_implementation_keras
+1. Put CSV file in annotation-csv file.
+2. Run allah.py for train up.
+3. Save training model in cv-rnn-model directory.
+4. Choose best weight and create a model.hdf5 file.
+5. Training time accuracy and loss graph store in Result-plot directory.
+6. Run allahone.py for test the best weight properly work with RNN features or not.
---
+++ allah.py
@@ -0,0 +1,97 @@
+#Import library
+import numpy
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.callbacks import ModelCheckpoint
+from keras.utils import np_utils
+# load ascii text and covert to lowercase
+filename = "/home/jishan/Desktop/TEST-F-RNN-D-FD/annotation-csv/tf.csv"
+raw_text = open(filename).read()
+raw_text = raw_text.lower()
+# create mapping of unique chars to integers
+chars = sorted(list(set(raw_text)))
+char_to_int = dict((c, i) for i, c in enumerate(chars))
+# summarize the loaded data
+n_chars = len(raw_text)
+n_vocab = len(chars)
+print "Total Characters: ", n_chars
+print "Total Vocab: ", n_vocab
+# prepare the dataset of input to output pairs encoded as integers
+seq_length = 100
+dataX = []
+dataY = []
+for i in range(0, n_chars - seq_length, 1):
+	seq_in = raw_text[i:i + seq_length]
+	seq_out = raw_text[i + seq_length]
+	dataX.append([char_to_int[char] for char in seq_in])
+	dataY.append(char_to_int[seq_out])
+n_patterns = len(dataX)
+print "Total Patterns: ", n_patterns
+# reshape X to be [samples, time steps, features]
+X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
+# normalize
+X = X / float(n_vocab)
+# one hot encode the output variable
+y = np_utils.to_categorical(dataY)
+# define the LSTM model
+model = Sequential()
+model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
+model.add(Dropout(0.2))
+model.add(Dense(y.shape[1], activation='softmax'))
+print(model.summary())
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=["accuracy"])
+# define the checkpoint
+filepath="/home/jishan/Desktop/TEST-F-RNN-D-FD/cv-rnn-model/weights-improvement-{epoch:02d}-{loss:.4f}-{acc:.4f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+callbacks_list = [checkpoint]
+# fit the model
+hist=model.fit(X, y, epochs=25, batch_size=128, callbacks=callbacks_list)
+print "Save model in directory"
+# summarize history for accuracy
+plt.plot(hist.history['acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['accuracy'], loc='upper left')
+plt.savefig('/home/jishan/Desktop/TEST-F-RNN-D-FD/Result-plot/train_accuracy_figure')
+print "Save accuracy graph in directory"
+plt.show()
+
+# summarize history for loss
+plt.plot(hist.history['loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['loss'], loc='upper right')
+plt.savefig('/home/jishan/Desktop/TEST-F-RNN-D-FD/Result-plot/train_loss_figure')
+print "Save loss graph in directory"
+plt.show()
+
+
+
+# Saving and loading model and weights
+from keras.models import model_from_json
+from keras.models import load_model
+
+# serialize model to JSON
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk")
+
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+
+model.save('model.hdf5')
+loaded_model=load_model('model.hdf5')
